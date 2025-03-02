1e841b6d565396ed36193807f10b9c34e28c63b7
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-03-03 02:49:22 +0700

    override <> display_info() to include engine capacity

--- rentalService.py
+++ rentalService.py
@@ -39,3 +39,6 @@ class Bike(Vehicle):
     def __init__(self,brand,model,year,rental_price_per_day,engine_capacity):
         super().__init__(brand,model,year,rental_price_per_day)
         self.engine_capacity = engine_capacity
+
+    def display_info(self):
+        print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.get_rental_price_per_day()}/day")
