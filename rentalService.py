9e6dd76c87d19bcd8fdd96fdd64bc7ddba2723f1
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-03-03 02:44:43 +0700

    override <> display_info() to include seating capacity (inherits from Vehicle)

--- rentalService.py
+++ rentalService.py
@@ -30,3 +30,6 @@ class Car(Vehicle):
         super().__init__(brand,model,year,rental_price_per_day)
         self.seating_capacity = seating_capacity
 
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.get_rental_price_per_day()}/day")
+
