b3e18d10ae3dc3268c879004825e9e8e6fc029b9
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-03-03 02:51:44 +0700

    create <> function show_vehicle_info(vehicle) that takes a Vehicle object and calls display_info()

--- rentalService.py
+++ rentalService.py
@@ -42,3 +42,8 @@ class Bike(Vehicle):
 
     def display_info(self):
         print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.get_rental_price_per_day()}/day")
+
+
+
+def show_vehicle_info(vehicle):
+    vehicle.display_info(self)
